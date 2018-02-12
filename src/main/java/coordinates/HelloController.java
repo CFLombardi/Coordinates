881a332a609d80393b539b78c91d2f01e3fa9e00
Tony <Sanzman12@yahoo.com>
2018-02-12 04:58:32 +0000

    Code clean up

--- src/main/java/coordinates/HelloController.java
+++ src/main/java/coordinates/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.validation.BindingResult;
 
 @Controller
 public class HelloController {
